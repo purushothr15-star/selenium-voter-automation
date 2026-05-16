c1d15c7f07bd56ab7d6d21f95949d02af2f84361
purushothr15-star <purushoth.r.15@gmail.com>
2026-05-16 17:48:11 +0530

    loading properties as class path

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,29 +1,37 @@
+
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
+
     private static Properties prop;
 
     static {
 
-        try
+        try {
 
-    {
-        FileInputStream fis = new FileInputStream("C:\\Users\\aishw\\PurushothProjects\\selenium-voter-automation\\src\\test\\resources\\globalProps.properties");
-        prop = new Properties();
-        prop.load(fis);
-    } catch(
-    IOException e)
+            InputStream input = ConfigReader.class
+                    .getClassLoader()
+                    .getResourceAsStream("globalProps.properties");
 
-    {
-        e.printStackTrace();
+            if (input == null) {
+                throw new RuntimeException("globalProps.properties not found");
+            }
+            prop = new Properties();
+
+            prop.load(input);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
-    }
 
-    public static String get(String key){
+    public static String get(String key) {
+
         return prop.getProperty(key);
     }
-    }
+}
